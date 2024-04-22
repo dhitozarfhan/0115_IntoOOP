b1e17aa2a9f399c4b272b605514800876b30c74e
dhitozarfhan <dhitozarfhan@gmail.com>
2024-04-22 15:38:25 +0700

    display

--- opp 2/opp 2.cpp
+++ opp 2/opp 2.cpp
@@ -2,3 +2,28 @@
 using namespace std;
 
 class BangunDatar {
+
+	//akses modifier
+private:
+	float panjang, lebar;
+public:
+	float luas;
+
+	void input() {
+
+		cout << "masukan panjangnya = ";
+		cin >> panjang;
+		cout << "masukkan lebarnya = ";
+		cin >> lebar;
+	}
+
+	float hitunganluas() {
+		return panjang * lebar;
+	}
+
+	void display() {
+		cout << "panjangnya = " << panjang << endl;
+		cout << "lebarnya  = " << panjang << endl;
+		cout << "luasnya = " << panjang << endl;
+	}
+
